6e714f3293e054dc6f55ca1c496d96004cf1893f
ChizimInc <cezarchizim4@gmail.com>
2026-01-08 14:07:33 +0200

    change path in next config file

--- apps/merchant/next.config.mjs
+++ apps/merchant/next.config.mjs
@@ -3,7 +3,7 @@ import path from "node:path";
 import { fileURLToPath } from "node:url";
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
-const withNextIntl = createNextIntlPlugin("./apps/merchant/next-intl.config.ts");
+const withNextIntl = createNextIntlPlugin("./next-intl.config.ts");
 
 
 const isProd = process.env.NODE_ENV === "production";
@@ -47,7 +47,7 @@ const nextConfig = {
   },
   experimental: {
     outputFileTracingIncludes: {
-      "/": ["./apps/merchant/next-intl.config.ts"],
+      "/": ["./next-intl.config.ts"],
     }
   },
   serverExternalPackages: [],
